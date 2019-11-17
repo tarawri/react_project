46f1101d16bad2b48ab1aae1acd798697509e679
tarawri <tarawri.dev@gmail.com>
2019-11-17 10:58:15 +0300

    Updates

---
+++ precache-manifest.c099f9dae5858492ae81591bf28de514.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949ac987b308f9a0d8b34f631ae055f1",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "e335e7e887a931337aa0",
+    "url": "/react_project/static/css/main.daa52dd4.chunk.css"
+  },
+  {
+    "revision": "7d9fdb3705bc04729f6c",
+    "url": "/react_project/static/js/2.48102a88.chunk.js"
+  },
+  {
+    "revision": "e335e7e887a931337aa0",
+    "url": "/react_project/static/js/main.580dd14a.chunk.js"
+  },
+  {
+    "revision": "373b2d83d369b18ad9ca",
+    "url": "/react_project/static/js/runtime-main.d412124e.js"
+  }
+]);
